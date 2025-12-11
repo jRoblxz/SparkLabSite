c828370c63d76d1c3e902ce7aaf906f48824d957
jRoblxz <joaoroblez76@gmail.com>
2025-12-11 06:46:49 -0300

    UP

--- spark-lab-code-site/client/src/pages/sections/Skills/SkillSection.tsx
+++ spark-lab-code-site/client/src/pages/sections/Skills/SkillSection.tsx
@@ -34,7 +34,7 @@ export function SkillsSection() {
           <h2 className="text-4xl md:text-5xl font-bold mb-12 text-center">
             My Skills
           </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 **justify-items-center** **md:justify-items-start**">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 ">
             <StyeledPHP />
             <StyeledLaravel />
             <StyledPython />
